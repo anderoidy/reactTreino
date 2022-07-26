9ba6d5d7cc102bac96e3f499beddf315ff89250b
anderoidy <anderoidy@gmail.com>
2022-07-26 01:52:06 -0300

    adicionado o css do componete game

--- 6_CAÇA_PALAVRAS/src/App.js
+++ 6_CAÇA_PALAVRAS/src/App.js
@@ -24,8 +24,48 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
 
+  const [pickedWord, setPickedWord] = useState("")
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([])
+
+  const pickWordAndCategory = () => {
+  // pick randow category
+    const categories = Object.keys(words)
+    const category = 
+      categories[Math.floor(Math.random() * Object.keys(categories).length)]
+
+      console.log(category)
+
+  // pick a random word
+    const word = 
+    words[category][Math.floor(Math.random() * words[category].length)]
+
+    console.log(word)
+
+    return {word, category}
+
+  }
+
+
+
   //start secrets word game
   const startGame = () => {
+    // pick word and pick category
+    const {word, category} = pickWordAndCategory()
+
+    // create an array os letters
+    let wordLetters = word.split("")
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase())
+
+    console.log(word, category)
+    console.log(wordLetters)
+
+    // fill states 
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(letters)
+
     setGameStage(stages[1].name)
   }
 
@@ -39,7 +79,7 @@ function App() {
     setGameStage(stages[0].name)
   }
 
-  console.log(words)
+  
  
   return (
     <div className="App">
--- 6_CAÇA_PALAVRAS/src/components/Game.js
+++ 6_CAÇA_PALAVRAS/src/components/Game.js
@@ -4,9 +4,26 @@ import "./Game.css"
 
 const Game = ({verifyLatter}) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLatter}>Finalizar Jogo</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Adivinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palacra: <span>Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button>Jogar!</button>
+
+        </form>
+      </div>
     </div>    
   )
 }
