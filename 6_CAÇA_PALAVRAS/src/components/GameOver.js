49ffbef557341ed60cd2792c41c8f68637e5829f
anderoidy <anderoidy@gmail.com>
2022-07-30 03:35:21 -0300

    finalizado projeto caça palavras

--- 6_CAÇA_PALAVRAS/src/components/GameOver.js
+++ 6_CAÇA_PALAVRAS/src/components/GameOver.js
@@ -1,10 +1,13 @@
 import "./GameOver.css"
 import React from "react"
 
-const GameOver = ({retry}) => {
+const GameOver = ({retry, score}) => {
   return (
-    <div>
+    <div className="gameover">
       <h1>Fim do Jogo</h1>
+      <h2>
+        A sua pontuação foi: <span>{score}</span>
+      </h2>
       <button onClick={retry}>Reinicializar o Jogo</button>
     </div>
   )
